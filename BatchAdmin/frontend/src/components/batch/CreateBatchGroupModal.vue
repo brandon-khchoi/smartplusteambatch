1fd6e88738c18b9221cb7be1014da2c7de7c7345
brandon <khchoi@lunasoft.co.kr>
2020-08-20 11:41:15 +0900

    [0000] 수정

--- BatchAdmin/frontend/src/components/batch/CreateBatchGroupModal.vue
+++ BatchAdmin/frontend/src/components/batch/CreateBatchGroupModal.vue
@@ -139,12 +139,13 @@ export default {
       item.luna_team_no = this.lunaTeamNo
 
       let tempDispluaNo = 0
+      console.log(this.batchGroupList)
       for (const idx in this.batchGroupList) {
-        if (tempDispluaNo < this.batchGroupList[idx].display_no) {
+        if (tempDispluaNo < Number(this.batchGroupList[idx].display_no)) {
           tempDispluaNo = this.batchGroupList[idx].display_no
         }
       }
-      item.display_no = tempDispluaNo + 1
+      item.display_no = Number(tempDispluaNo) + 1
       this.batchGroupList.push(item)
     },
     deleteBatchGroup (groupNo, idx) {
@@ -154,7 +155,8 @@ export default {
         http
           .get('/batchManage/deleteBatchGroup', {
             params: {
-              groupNo: groupNo
+              groupNo: groupNo,
+              lunaNo: this.lunaNo
             }
           })
           .then(result => {
