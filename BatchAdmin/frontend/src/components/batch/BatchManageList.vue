0007088d89cbfcb0783c62712682cadf102ee665
brandon <khchoi@lunasoft.co.kr>
2020-11-19 14:12:10 +0900

    dept_no 하드 코딩 수정

--- BatchAdmin/frontend/src/components/batch/BatchManageList.vue
+++ BatchAdmin/frontend/src/components/batch/BatchManageList.vue
@@ -5,7 +5,7 @@
         <template v-if="realLunaTeamNo === 19">
           <select v-model="lunaTeamNo" style="width:10%" @change="teamChange()">
             <template v-for="(val, code) in codeData.lunaTeamInfo">
-              <option :key="code" :value="Number(val.luna_team_no)" v-if="val.luna_dept_no === '2'">{{ val.luna_team_name }}</option>
+              <option :key="code" :value="Number(val.luna_team_no)" v-if="val.luna_dept_no === '2' || val.luna_dept_no === '12'">{{ val.luna_team_name }}</option>
             </template>
             <option value="999">리로드</option>
             <option value="9999">스마트플러스 테스트</option>
