44a427cf6045ff948956cef870c280f02eb12a27
brandon <khchoi@lunasoft.co.kr>
2021-05-12 17:45:42 +0900

    [14044] 몽고DB 배치로그 월별로 적재 인덱스 추가

--- BatchAdmin/src/main/java/kr/co/lunasoft/batchadmin/vo/log/batch/BatchLogMongo.java
+++ BatchAdmin/src/main/java/kr/co/lunasoft/batchadmin/vo/log/batch/BatchLogMongo.java
@@ -3,6 +3,8 @@ package kr.co.lunasoft.batchadmin.vo.log.batch;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -13,19 +15,20 @@ import java.util.Date;
 @Document(collection = "batch_log_#{T(kr.co.lunasoft.batchadmin.common.MongoIndex).getIndexDate()}")
 public class BatchLogMongo {
 
-	private Date log_date;
-	private int batch_no;
-	private int batch_result_type_code;
-	private Date batch_start_date;
-	private String batch_start_date_desc;
-	private Date batch_end_date;
-	private String batch_end_date_desc;
-	private String batch_fail_text;
+    @Indexed(name = "log_date_-1", direction = IndexDirection.DESCENDING)
+    private Date log_date;
+    private int batch_no;
+    private int batch_result_type_code;
+    private Date batch_start_date;
+    private String batch_start_date_desc;
+    private Date batch_end_date;
+    private String batch_end_date_desc;
+    private String batch_fail_text;
 
-	public void setBatch_fail_text(String batch_fail_text) {
-		if(!"".equals(batch_fail_text)) {
-			this.batch_fail_text = batch_fail_text;
-		}
-	}
+    public void setBatch_fail_text(String batch_fail_text) {
+        if (!"".equals(batch_fail_text)) {
+            this.batch_fail_text = batch_fail_text;
+        }
+    }
 
 }
